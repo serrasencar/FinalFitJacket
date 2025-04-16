8518dceccd7b7d843db8ea5f26798e56c9fa9477
VidhiKulkarni <32562016+VidhiKulkarni@users.noreply.github.com>
2025-04-16 10:27:29 -0700

    Added badges

--- fitjacket/fitjacket/settings.py
+++ fitjacket/fitjacket/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -144,4 +145,4 @@ EMAIL_HOST_PASSWORD = 'gnku bpif qyjh begd'  # Not your Gmail password!
 DEFAULT_FROM_EMAIL = 'FitJacket <fitjacket.no.reply@gmail.com>'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
